89dbdf6908de4b828e22addf6602ab85663fa789
Juliano Tavares <juliano.tavaress@gmail.com>
2023-01-26 10:10:11 -0300

    fix: fix the setSystemToken logic for conversion

--- android/src/main/java/com/salesforce/marketingcloud/reactnative/RNMarketingCloudSdkModule.java
+++ android/src/main/java/com/salesforce/marketingcloud/reactnative/RNMarketingCloudSdkModule.java
@@ -33,14 +33,14 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.salesforce.marketingcloud.MCLogListener;
 import com.salesforce.marketingcloud.MarketingCloudSdk;
 import com.salesforce.marketingcloud.events.EventManager;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import javax.annotation.Nonnull;
 
@@ -91,6 +91,26 @@ public class RNMarketingCloudSdkModule extends ReactContextBaseJavaModule {
             @Override
             void execute(MarketingCloudSdk sdk, @NonNull Promise promise) {
                 promise.resolve(sdk.getPushMessageManager().getPushToken());
+            }
+        });
+    }
+
+    @ReactMethod
+    public void setSystemToken(final String token) {
+        handleAction(new Action() {
+            @Override
+            void execute(MarketingCloudSdk sdk) {
+                sdk.getPushMessageManager().setPushToken(token);
+            }
+        });
+    }
+
+    @ReactMethod
+    public void getDeviceID(Promise promise) {
+        handleAction(new PromiseAction(promise) {
+            @Override
+            void execute(MarketingCloudSdk sdk, @NonNull Promise promise) {
+                promise.resolve(sdk.getRegistrationManager().getDeviceId());
             }
         });
     }
@@ -207,7 +227,7 @@ public class RNMarketingCloudSdkModule extends ReactContextBaseJavaModule {
             void execute(MarketingCloudSdk sdk) {
                 try {
                     log("~#RNMCSdkModule", "SDK State: " + sdk.getSdkState().toString(2));
-                } catch(Exception e) {
+                } catch (Exception e) {
                     // NO-OP
                 }
             }
@@ -220,9 +240,10 @@ public class RNMarketingCloudSdkModule extends ReactContextBaseJavaModule {
             @Override
             void execute(MarketingCloudSdk sdk) {
                 try {
-                    sdk.getEventManager().track(EventManager.customEvent(name, attributes.toHashMap()));
+                    sdk.getEventManager().track(
+                        EventManager.customEvent(name, attributes.toHashMap()));
                     log("~#RNMCSdkModule", name + " Event Tracked.");
-                } catch(Exception e) {
+                } catch (Exception e) {
                     log("~#RNMCSdkModule", "Error Tracking Event: " + e.getMessage());
                 }
             }
