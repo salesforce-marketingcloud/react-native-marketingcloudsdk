b47d7221a4145094dcc5bff3c0c666113392c9e5
Bill Mote <bmote@salesforce.com>
2024-09-23 12:00:54 -0400

    @W-16709624 - PR Comments

--- example/android/app/src/main/java/com/salesforce/marketingcloud/react/MainApplication.kt
+++ example/android/app/src/main/java/com/salesforce/marketingcloud/react/MainApplication.kt
@@ -17,6 +17,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 import com.salesforce.marketingcloud.MarketingCloudConfig
+import com.salesforce.marketingcloud.UrlHandler
 import com.salesforce.marketingcloud.notifications.NotificationCustomizationOptions
 import com.salesforce.marketingcloud.notifications.NotificationManager
 import com.salesforce.marketingcloud.notifications.NotificationMessage
@@ -25,122 +26,104 @@ import com.salesforce.marketingcloud.sfmcsdk.SFMCSdk
 import com.salesforce.marketingcloud.sfmcsdk.SFMCSdkModuleConfig
 import kotlin.random.Random
 
-class MainApplication : Application(), ReactApplication {
-    override val reactNativeHost: ReactNativeHost =
-        object : DefaultReactNativeHost(this) {
-            override fun getPackages(): List<ReactPackage> =
-                PackageList(this).packages.apply {
-                    // Packages that cannot be autolinked yet can be added manually here, for example:
-                    // add(MyReactNativePackage())
-                }
 
-            override fun getJSMainModuleName(): String = "index"
-
-            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+class MainApplication : Application(), ReactApplication, UrlHandler,
+  NotificationManager.NotificationChannelIdProvider, NotificationManager.NotificationLaunchIntentProvider {
+  override val reactNativeHost: ReactNativeHost =
+    object : DefaultReactNativeHost(this) {
+      override fun getPackages(): List<ReactPackage> =
+        PackageList(this).packages.apply {
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // add(MyReactNativePackage())
         }
 
-    override val reactHost: ReactHost
-        get() = getDefaultReactHost(applicationContext, reactNativeHost)
+      override fun getJSMainModuleName(): String = "index"
 
-    override fun onCreate() {
-        super.onCreate()
-        SoLoader.init(this, false)
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            // If you opted-in for the New Architecture, we load the native entry point for this app.
-            load()
-        }
+      override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-        SFMCSdk.configure(
-            applicationContext,
-            SFMCSdkModuleConfig.build {
-                pushModuleConfig =
-                    MarketingCloudConfig.builder()
-                        .apply {
-                            //Update these details based on your MC config
-                            setApplicationId("{MC_APP_ID}")
-                            setAccessToken("{MC_ACCESS_TOKEN}")
-                            setMarketingCloudServerUrl("{MC_APP_SERVER_URL}")
-                            setSenderId("{FCM_SENDER_ID}")
-                            setNotificationCustomizationOptions(
-                                NotificationCustomizationOptions.create(
-                                    R.mipmap.ic_launcher,
-                                    { context, notificationMessage ->
-                                        val requestCode = Random.nextInt()
-                                        val url = notificationMessage.url
-                                        when {
-                                            url.isNullOrEmpty() ->
-                                                PendingIntent.getActivity(
-                                                    context,
-                                                    requestCode,
-                                                    Intent(context, MainActivity::class.java),
-                                                    provideIntentFlags(),
-                                                )
-                                            else ->
-                                                PendingIntent.getActivity(
-                                                    context,
-                                                    requestCode,
-                                                    Intent(Intent.ACTION_VIEW, Uri.parse(url)),
-                                                    provideIntentFlags(),
-                                                )
-                                        }
-                                    },
-                                    { context, notificationMessage ->
-                                        NotificationManager.createDefaultNotificationChannel(context)
-                                    },
-                                ),
-                            )
-                            setUrlHandler(
-                                { context, url, _ ->
-                                    PendingIntent.getActivity(
-                                        context,
-                                        Random.nextInt(),
-                                        Intent(Intent.ACTION_VIEW, Uri.parse(url)),
-                                        provideIntentFlags(),
-                                    )
-                                },
-                            )
-                        }
-                        .build(applicationContext)
+      override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+      override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+    }
+
+  override val reactHost: ReactHost
+    get() = getDefaultReactHost(applicationContext, reactNativeHost)
+
+  override fun onCreate() {
+    super.onCreate()
+    SoLoader.init(this, false)
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      load()
+    }
+
+    SFMCSdk.configure(
+      applicationContext,
+      SFMCSdkModuleConfig.build {
+        pushModuleConfig =
+          MarketingCloudConfig.builder()
+            .apply {
+              //Update these details based on your MC config
+              setApplicationId("{MC_APP_ID}")
+              setAccessToken("{MC_ACCESS_TOKEN}")
+              setMarketingCloudServerUrl("{MC_APP_SERVER_URL}")
+              setSenderId("{FCM_SENDER_ID}")
+              setNotificationCustomizationOptions(
+                NotificationCustomizationOptions.create(
+                  R.mipmap.ic_launcher,
+                  this@MainApplication,
+                  this@MainApplication,
+                ),
+              )
+              setUrlHandler(this@MainApplication)
             }
-        ) { initStatus ->
-            when (initStatus.status) {
-                InitializationStatus.SUCCESS -> Log.d("SFMC", "SFMC SDK Initialization Successful")
-                InitializationStatus.FAILURE -> Log.d("SFMC", "SFMC SDK Initialization Failed")
-                else -> Log.d("SFMC", "SFMC SDK Initialization Status: Unknown")
-            }
-        }
+            .build(applicationContext)
+      }
+    ) { initStatus ->
+      when (initStatus.status) {
+        InitializationStatus.SUCCESS -> Log.d("SFMC", "SFMC SDK Initialization Successful")
+        InitializationStatus.FAILURE -> Log.d("SFMC", "SFMC SDK Initialization Failed")
+        else -> Log.d("SFMC", "SFMC SDK Initialization Status: Unknown")
+      }
     }
+  }
 
-    private fun getNotificationPendingIntent(
-        context: Context,
-        notificationMessage: NotificationMessage,
-    ): PendingIntent {
-        return when {
-            notificationMessage.url.isNullOrEmpty() ->
-                PendingIntent.getActivity(
-                    context,
-                    Random.nextInt(),
-                    context.packageManager.getLaunchIntentForPackage(context.packageName),
-                    provideIntentFlags(),
-                )
-            else ->
-                PendingIntent.getActivity(
-                    context,
-                    Random.nextInt(),
-                    Intent(Intent.ACTION_VIEW, Uri.parse(notificationMessage.url)),
-                    provideIntentFlags(),
-                )
-        }
-    }
+  override fun getNotificationChannelId(context: Context, notificationMessage: NotificationMessage): String {
+    return NotificationManager.createDefaultNotificationChannel(context)
+  }
 
-    private fun provideIntentFlags(): Int {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-        } else {
-            PendingIntent.FLAG_UPDATE_CURRENT
-        }
+  override fun handleUrl(context: Context, url: String, urlSource: String): PendingIntent? {
+    return getPendingIntent(context, url)
+  }
+
+  override fun getNotificationPendingIntent(context: Context, notificationMessage: NotificationMessage): PendingIntent {
+    return getPendingIntent(context, notificationMessage.url)
+  }
+
+  private fun getPendingIntent(context: Context, url: String?): PendingIntent {
+    return when {
+      url.isNullOrEmpty() ->
+        PendingIntent.getActivity(
+          context,
+          Random.nextInt(),
+          context.packageManager.getLaunchIntentForPackage(context.packageName),
+          provideIntentFlags(),
+        )
+
+      else ->
+        PendingIntent.getActivity(
+          context,
+          Random.nextInt(),
+          Intent(Intent.ACTION_VIEW, Uri.parse(url)),
+          provideIntentFlags(),
+        )
     }
+  }
+
+  private fun provideIntentFlags(): Int {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+      PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    } else {
+      PendingIntent.FLAG_UPDATE_CURRENT
+    }
+  }
 }
